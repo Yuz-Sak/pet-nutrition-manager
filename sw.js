085e9ff1a96361941b0dda9e31336b759e39d216
shintaro <exp.log.kondo@gmail.com>
2026-09-07 00:14:40 +0900

    Force service worker refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pet-nutrition-v3';
+const CACHE_NAME = 'pet-nutrition-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -9,10 +9,12 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
       keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
