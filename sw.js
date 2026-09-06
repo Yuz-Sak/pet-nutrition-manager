16c246c6d7ac7a2828c0e11fd6e38687f88d5d43
shintaro <exp.log.kondo@gmail.com>
2026-09-06 23:53:08 +0900

    Refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
-const CACHE_NAME = 'pet-nutrition-v1';
+const CACHE_NAME = 'pet-nutrition-v2';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './ingredients.csv'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
